c531338b9f41334cca319338450b98714687abe4
soravit_ice <61050302@kmitl.ac.th>
2025-09-28 01:25:29 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: config.corsOrigin,
+    origin: "https://incandescent-pavlova-dc36ec.netlify.app",
     credentials: false
   })
 );
